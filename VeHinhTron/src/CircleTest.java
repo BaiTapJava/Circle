d516b015441755ba50d58a90fabe8087398c49a3
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-10-12 18:22:46 +0700

    Ve Hinh Tron OOP

---
+++ VeHinhTron/src/CircleTest.java
@@ -0,0 +1,9 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3, "Yellow");
+
+        System.out.println(circle.toString());
+
+
+    }
+}
---
+++ VeHinhTron/src/CylinderTest.java
@@ -0,0 +1,7 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(5,"Blue");
+
+        System.out.println(cylinder.toString());
+    }
+}
